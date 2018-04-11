7b6d50c4121e8688e30b032f3a59dbaf4d09090b
kristinlin <klin11@stuy.edu>
2018-04-11 14:03:03 -0400

    your basic flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, flash, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
