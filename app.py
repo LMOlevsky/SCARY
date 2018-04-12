061b04d3641a4620c01977abdea0371d77497c82
Kristin Lin <klin11@stuy.edu>
2018-04-12 13:30:10 -0400

    linked to flask

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    return render_template('testmap.html')
 
 if __name__ == "__main__":
     app.debug = True
