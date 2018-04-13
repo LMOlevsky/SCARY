95575b820cf8a9ed9a3676f702020ddacfac884c
sonalparab <sparab@stuy.edu>
2018-04-13 01:31:24 -0400

    add state ids

--- data/csvToDict.py
+++ data/csvToDict.py
@@ -1,6 +1,25 @@
 import csv
 
-def csvToDict(filename):
+def csvToDictStates(file):
+    source = open(file, 'rU')
+    body = source.read()
+    source.close() 
+    listofcontents = body.split('\n')
+    listofcontents.pop()
+    titles = listofcontents.pop(0).split(',')
+    dict = {}
+    
+    for element in listofcontents:
+        organizedlist = element.split(',')
+        #print organizedlist
+
+        dict[organizedlist[1].upper()] = int(organizedlist[0])
+
+    return dict
+	
+stateIDs = csvToDictStates('states')
+
+def csvToDictCities(filename):
     source = open(filename, 'rU')
     body = source.read()
     source.close() 
@@ -13,12 +32,12 @@ def csvToDict(filename):
 
     #to keep track of state, because it is only written once
     state = listofcontents[0].split(',')[0]
-    print state
+    #print state
     for element in listofcontents:
-        #ALABAMA,BIRMINGHAM,2016,"212,549","1,732",44,75,460,"1,153","5,875","1,318","3,807",750,76
         #print element
+        
         #to remove the extra commas in numbers
-        # using the quotes to find themxs
+        # using the quotes to find them
         quote = element.find('"');
         while(quote != -1):
             end = element.find('"',quote+1)
@@ -35,7 +54,7 @@ def csvToDict(filename):
         else:
             state = organizedlist[0]
             subdict['State'] = state
-            print state
+            #print state
 
         #add the rest of the data
         position = 1
@@ -44,24 +63,33 @@ def csvToDict(filename):
             if(organizedlist[2] == '2017'):
                 break
             data = organizedlist[position]
-            #print data
-            #if(data.find('@') != -1):
-            #    data = data.replace('@','')
+
+            #add numbers as integers
             try:
                 subdict[titles[position]] = int(data)
             except:
                 subdict[titles[position]] = data
-            #print position
             position += 1
+        
+        #add the state's id for map
+        try:
+            subdict["ID"] = stateIDs[state]
+        except:
+            if(state == 'DISTRICT OF COLUMBIA'):
+                subdict["ID"] = 51
+
+        #add the city to the dictionary
         dict[organizedlist[1]] = subdict
     return dict
-	
-d0 = csvToDict('alabama-through-california.csv')
-d1 = csvToDict('colorado-through-hawaii.csv')
-d2 = csvToDict('illinois-through-missouri.csv')
-d3 = csvToDict('montana-through-ohio.csv')
-d4 = csvToDict('oklahoma-through-wisconsin.csv')
 
+#make a dictionary of all the given data
+d0 = csvToDictCities('alabama-through-california.csv')
+d1 = csvToDictCities('colorado-through-hawaii.csv')
+d2 = csvToDictCities('illinois-through-missouri.csv')
+d3 = csvToDictCities('montana-through-ohio.csv')
+d4 = csvToDictCities('oklahoma-through-wisconsin.csv')
+
+#combine all the cities
 all = dict(d0.items() + d1.items() + d2.items() + d3.items() + d4.items())
 
 #to remove empty key
@@ -69,8 +97,6 @@ try:
     del all[""]
 except:
     print
-    
-print
 
 #to create a csv to use for the database
 def writeData(d):
@@ -78,10 +104,7 @@ def writeData(d):
         w = csv.writer(f)
         w.writerow(d['NEW YORK'].keys())
         for key in d:
-            #print
-            print key
-            if key == "WISCONSIN":
-                print key
             w.writerow(d[key].values())
 
 writeData(all)
+
