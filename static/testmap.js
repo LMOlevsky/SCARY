483102d53780b6c01d14659babe11fa76e3a2f70
Kristin Lin <klin11@stuy.edu>
2018-04-12 23:16:46 -0400

    color changes to blue

--- static/testmap.js
+++ static/testmap.js
@@ -1,6 +1,13 @@
 var svg = d3.select("svg");
 var path = d3.geoPath();
 
+var states=[];
+for (var i = 0; i < 50; i++) {
+    states.push((Math.floor((Math.random()*100)))*0.01);
+}
+console.log(states);
+
+    
 d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
     if (error) throw error;
 
@@ -15,9 +22,16 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
     console.log("HEY");
     d3.selectAll("path")
 	.style("fill", function(d, i) {
-	    console.log(i);
-	    return "rgb(" + (Math.floor((Math.random()*50)))*8 + ",0,0)";
-	});
+//	    console.log((Math.floor((Math.random()*100)))*0.01);
+	    return "rgba(255,0,0,"+ states[i] +")" ;
+	})
+	.on('mouseover', function(d, i) {
+	    hovering(i);
+	})
+/*	.on('mouseout', function() {
+	    console.log("You good?");
+	    hovering(-1);
+	});*/
 
     //apply state borders attributes
     svg.append("path")
@@ -26,3 +40,16 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
 
 
 });
+
+var hovering = function(state) {
+    d3.selectAll("path")
+	.style("fill", function(d, i) {
+	    if (i == state) {
+		console.log("once");
+		return "blue";
+	    } else {
+		return "rgba(255,0,0,"+ states[i] +")";
+	    }
+	});
+
+};
