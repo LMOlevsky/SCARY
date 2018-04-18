0a6331e1ce11428108b52d1929fabeb2695d9dff
Jenny Gao <jgao658@gmail.com>
2018-04-17 21:52:10 -0400

    worked on hover and bars

--- static/testmap.js
+++ static/testmap.js
@@ -1,6 +1,7 @@
 var svg = d3.select("svg");
 var path = d3.geoPath();
 
+/*
 var color = function(tag){
     if (tag == "murder"){
 	return "rgba(255,0,0,";
@@ -18,7 +19,7 @@ var color = function(tag){
 	return "rgba(255,255,0,";
     }
 };
-
+*/
 //==================================================
 //Configuring transparencies
 
@@ -50,6 +51,8 @@ var alphas = function(tag) {
 
 //==================================================
 //Creating the states
+var back = svg.append("g"); // appended first
+var front = svg.append("g");
 
 var needs_work = function(tag) {
 
@@ -59,7 +62,8 @@ var needs_work = function(tag) {
 	if (error) throw error;
 
 	//create states and apply states attrs
-	svg.append("g")
+	//svg.append("g")
+	back
 	    .attr("class", "states")
 	    .selectAll("path")
 	//binded data has data for each state; each index corresponds to a state
@@ -104,12 +108,103 @@ var hovering = function(state, states) {
 		//if it's negative; it means theres nothing there
 		return "blue";
 	    } else {
-		return  color(tag) + states[i] +")";
+		return "rgba(255,0,0,"+ states[i] +")";
 	    }
 	});
 
 };
 
+needs_work("murder");
 
 
-needs_work("rape");
+//var coords=[[100,500,100,200],[200,500,150,200]]
+var stateCoor=[[100,500],[200,500]]
+var coords=[[100,500,100,"murder"],[111,500,200,"arson"],[200,500,150,"murder"],[211,500,200,"arson"]]
+
+
+//IDEA: when user selects say 2 types of crimes, we need to append [xcor,ycor,heightOfBar,typeOfCrimeSelected] to "coords list"
+//for each state, check how many lists in "coords" already have those x and y cors and then generate updated xcor (+i*11) for this current bar
+
+/*
+function howManyTimes(state){
+    xCor=coords[state][0]
+    yCor=coords[state][1]
+    count=0
+    for(i=0;i<coords.length;i++){
+	if(coords[i][0]==xCor && coords[i][1]==yCor){
+	    count+=1
+	}
+    }
+    return count
+}
+//e.g. howManyTimes(0) is for Arkansas
+*/
+
+
+//var circles=svg.append("g").selectAll("circle").data(coords).enter()
+var circles=front.selectAll("circle").data(coords).enter()
+circles.append("circle").attr("cx",function(d){return d[0];})
+    .attr("cy",function(d){return d[1];})
+    .attr("r",function(d){return 5;})
+    .style("fill",function(d){return "blue";});
+
+function getCentroid(selection) {
+    // get the DOM element from a D3 selection
+    // you could also use "this" inside .each()
+    var element = selection.node(),
+        // use the native SVG interface to get the bounding box
+        bbox = element.getBBox();
+    // return the center of the bounding box
+    return [bbox.x + bbox.width/2, bbox.y + bbox.height/2];
+}
+
+//console.log(getCentroid(svg.selectAll("path")));
+//console.log(svg.selectAll("path"));
+
+var chart = d3.select(".chart").attr("width",960).attr("height",600);
+var bar = chart.selectAll("rect");
+var barUpdate = bar.data(coords);
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .html(function(d) {
+    return "<strong>"+d[3]+": </strong> <span style='color:red'>" + d[2] + "</span>";
+  })
+
+svg.call(tip);
+
+/*
+var times=coords[0].length-2
+for(i=0;i<times;i++){
+    var barEnter = barUpdate.enter().append("rect");
+    barEnter.text(function(d) { return d; });
+	
+IGNORE---
+barEnter.style("height", function(d) {return d[2] + "px"; })
+    .style("top",function(d){return 600-d[1]+"px";})
+    .style("left",function(d){return d[0]+"px";});
+----
+
+    barEnter.attr("height", function(d) {return d[i+2]; })
+	.attr("width", function(d){return 10;})
+	.attr("y",function(d){return d[1]-d[i+2];})
+	.attr("x",function(d){return d[0]+i*11;})
+	.attr("class", "bar")
+    //.on('mouseover', tip.show)
+	.on('mouseover', tip.show)
+	.on('mouseout', tip.hide);
+}
+*/
+
+var barEnter = barUpdate.enter().append("rect");
+barEnter.text(function(d) { return d; });
+barEnter.attr("height", function(d) {return d[2]; })
+	.attr("width", function(d){return 10;})
+	.attr("y",function(d){return d[1]-d[2];})
+	.attr("x",function(d){return d[0];})
+	.attr("class", "bar")
+    //.on('mouseover', tip.show)
+	.on('mouseover', tip.show)
+	.on('mouseout', tip.hide);
+
+//needs_work("murder");
